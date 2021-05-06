eabccb55c6b3e6fb61519f566f3209a236b9657e
Luan Freitas <luannr16@gmail.com>
2021-05-05 22:06:12 -0300

    Create console application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AgendaConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
