398709c77729d3b44fed2256dee9d187feb7616b
Gabriel Nalin <n.gabriel@outlook.com.br>
2021-06-10 08:33:35 -0300

    adicionada DbContext

---
+++ Data/DbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AgendaConsoleApp.Models;
+
+namespace AgendaConsoleApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Entity> Entites { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
